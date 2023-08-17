372927f0dae9858850d382f757db2ce0355d612b
gilangfatahilah <gilangf010@gmail.com>
2023-08-17 20:59:47 +0700

    Membuat View Halaman Utama

--- app/Views/template_v.php
+++ app/Views/template_v.php
@@ -5,7 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ORTA</title>
+    <!-- Bootstrap  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Data Aos -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- my css -->
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -15,6 +18,13 @@
     <?php $this->renderSection('content'); ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <!-- Data Aos -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <!-- Aos Initialize -->
+    <script>
+        AOS.init();
+        AOS.refresh();
+    </script>
 </body>
 
 </html>
