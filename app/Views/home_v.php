49f53782279ee740bf5504937f4cd7c7554dd631
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2023-12-25 15:26:11 +0700

    feat: optimizing Ui and Langs

--- app/Views/home_v.php
+++ app/Views/home_v.php
@@ -31,17 +31,14 @@
 <section id="home" class="min-vh-100">
     <div class="row w-100" style="padding-top: 70px;">
         <div class="col-lg-6 col-md-6 col-sm-12 order-md-1 d-grid align-content-center ps-5 mb-4 mt-5">
-            <h1 class="p-color fs-heading fw-medium" data-aos="fade-down" data-aos-duration="2000">PT Karya Bumimas Persada</h1>
+            <h1 class="p-color fs-heading fw-medium mx-auto w-lg-85" data-aos="fade-down" data-aos-duration="2000">PT Karya Bumimas Persada</h1>
             <!-- data-aos="fade-down" data-aos-duration="2000" data-aos-delay="200" -->
             <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="200">
-                <p class="p-color mb-3 align-justify w-85" data-i18n="nav_home_p1">
-                    PT Karya Bumimas Persada merupakan perusahaan yang membuat Ceramics Sanitary dengan merek ORTA yang telah berdiri sejak tahun 2012 di Kabupaten Bogor, Jawa Barat, Indonesia.
-                </p>
-                <p class="p-color mb-3 align-justify w-85" data-i18n="nav_home_p2">
-                    ORTA mengeluarkan produk unggulan yaitu Squat Toilet (Kloset Jongkok). Selain produk tersebut ORTA juga mengeluarkan Toilet Flush (Kloset duduk tangki/siram) dan akan segera mengeluarkan produk Wastafel.
-                </p>
-                <p class="p-color mb-3 align-justify w-85" data-i18n="nav_home_p3">
-                    Produk kami mengutamakan kualitas yang baik agar pelanggan puas menggunakan produk ORTA dan wajib menjadi kloset pilihan anda yang dapat diaplikasikan dalam kehidupan sehari-hari.
+                <p class="p-color mb-3 align-justify mx-auto w-85" data-i18n="nav_home_p1"></p>
+                <p class="p-color mb-3 align-justify mx-auto w-85" data-i18n="nav_home_p2"></p>
+                <p class="p-color mb-3 align-justify mx-auto w-85" data-i18n="nav_home_p3"></p>
+                <p class="p-color mb-3 align-justify mx-auto w-85">
+                    ORTA <br> Everyday with Orta
                 </p>
             </div>
             <!-- <a href="/about" class="btn btn-secondary w-50 py-2 rounded-pill" data-aos="fade-down" data-aos-duration="2000" data-aos-delay="200">Selengkapnya</a> -->
@@ -49,12 +46,10 @@
                 <img src="<?= base_url('assets/image/cover/ornament.png'); ?>" class="img-fluid" alt="Ornament" width="200px">
             </div> -->
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 order-md-2 d-grid align-content-center px-5 mb-5 mt-5" data-aos="zoom-in" data-aos-duration="1000">
+        <div class="col-lg-6 col-md-6 col-sm-12 order-md-2 d-grid align-content-center px-5 my-5" data-aos="zoom-in" data-aos-duration="1000">
             <div class="ratio ratio-16x9">
-                <video controls autoplay loop class="rounded">
-                    <source src="<?= base_url('assets/vidio/Home Website.mp4'); ?>" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
+                <video controls autoplay loop class="rounded text-center">
+                    <source src="<?= base_url('assets/vidio/Home Website.mp4'); ?>" type="video/mp4" data-i18n="video_warning"></video>
             </div>
         </div>
     </div>
